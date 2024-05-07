64f1ea78f9da612a32b0e4fa0dc2eaa24200511d
LethalGerard <lethal_kim@hotmail.com>
2024-05-07 10:16:04 +0200

    Changes in appsettings.json

--- OrderMenu.api/ChatService.cs
+++ OrderMenu.api/ChatService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel;
+using OrderMenu.api.Plugins;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace OrderMenu.api
 {
@@ -8,68 +10,38 @@ namespace OrderMenu.api
     {
         readonly Kernel _kernel;
         readonly HttpClient _http;
-        ChatHistory chatHistory = [];
+        ChatHistory _chatHistory = [];
 
         public ChatService(Kernel kernel, HttpClient http)
         {
             _kernel = kernel;
             _http = http;
+            _kernel.ImportPluginFromType<ItemPlugin>();
+            _chatHistory.AddSystemMessage(File.ReadAllText("AiBotPrompt.txt"));
         }
 
         public async Task<string> ChatResponse(string input)
         {
-            var foodResponseFromApi = await _http.GetFromJsonAsync<List<Food>>("https://iths-2024-recept-grupp9-40k2zx.reky.se/recipes") ?? new List<Food>();
-            
-            //var drinkResponseFromApi = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
-           /* const category = [ "Beer", "Ordinary Drink", "Soft Drink", "Homemade Liqueur",];*/
-           // var cocktailResponseFromApi = await _http.GetFromJsonAsync<List<Drinks>>("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}");
-
-            var sides = foodResponseFromApi.Where(f => f.categories.Contains("Sides"));
-            var mains = foodResponseFromApi.Where(f => !f.categories.Contains("Sides"));
-
-            string mainsStringifyList = "List of mains:\n";
-
-            foreach(var main in mains) 
-            { 
-                mainsStringifyList += $"title:{main.title}, price: {main.price} sek.\n";
-            }
-
-            string sidesStringifyList = "List of sides:\n";
-
-            foreach (var side in sides)
+            var executionSettings = new OpenAIPromptExecutionSettings()
             {
-                sidesStringifyList += $"title:{side.title}, price: {side.price} sek.\n";
-            }
-            /*
-            foreach(var drink in drinks){
-                var drinkStringifyList="List of drinks"
-                drinkStringifyList+="$name, price "                
-                }
-                */
-            //Give personality & limitations
-            chatHistory.AddSystemMessage("You are a very angry assistant in Kuchisabichii asian food application, you hate your job.");
-            //chatHistory.AddSystemMessage($"Give recommendations for mains only from this list: {mainsStringifyList}");
-            //chatHistory.AddSystemMessage($"Give recommendations for sides only from this list: {sidesStringifyList}");
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
 
-            chatHistory.AddSystemMessage($"Give recommendations for mains only in {mainsStringifyList} and sides only in {sidesStringifyList}. ");
+            
 
-            chatHistory.AddUserMessage(input);
+            //_chatHistory.AddSystemMessage($"Give recommendations for mains only in {mainsStringifyList} and sides only in {sidesStringifyList}. ");
+
+            _chatHistory.AddUserMessage(input);
             var chatCompletion = _kernel.GetRequiredService<IChatCompletionService>();
-            var response = await chatCompletion.GetChatMessageContentAsync(chatHistory);
-            chatHistory.AddAssistantMessage(response.ToString());
+            var response = await chatCompletion.GetChatMessageContentAsync(_chatHistory, executionSettings, _kernel);
+            _chatHistory.AddAssistantMessage(response.ToString());
 
-            return response.ToString();
+
+            return response.Content;
         }
     }
     
-    public class Food 
-    {
-        public string _id { get; set; } = string.Empty;
-        public string title { get; set; } = string.Empty;
-        public int price { get; set; }
-        public string description { get; set; } = string.Empty;
-        public List<string> categories { get; set; } = [];
-    }
+
 
     // public class Drinks{
 
---
+++ OrderMenu.api/Plugins/ItemPlugin.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+using static System.Net.WebRequestMethods;
+
+namespace OrderMenu.api.Plugins;
+
+public class ItemPlugin
+{
+    [KernelFunction, Description("returns a list of drinks")] 
+    public string GetDrinksPlugin()
+    {
+        var result = System.IO.File.ReadAllText("Drinks.txt");
+        return result;
+    }
+
+    [KernelFunction, Description("returns a list of food items")]
+    public async Task<string> GetFoodPlugin()
+    {
+        var _http = new HttpClient();
+
+        var foodResponseFromApi = await _http.GetFromJsonAsync<List<Food>>("https://iths-2024-recept-grupp9-40k2zx.reky.se/recipes") ?? new List<Food>();
+
+        //var drinkResponseFromApi = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+        /* const category = [ "Beer", "Ordinary Drink", "Soft Drink", "Homemade Liqueur",];*/
+        // var cocktailResponseFromApi = await _http.GetFromJsonAsync<List<Drinks>>("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}");
+
+        var sides = foodResponseFromApi.Where(f => f.categories.Contains("Sides"));
+        var mains = foodResponseFromApi.Where(f => !f.categories.Contains("Sides"));
+
+        string mainsStringifyList = "List of mains:\n";
+
+        foreach (var main in mains)
+        {
+            mainsStringifyList += $"title:{main.title}, price: {main.price} sek.\n";
+        }
+
+        string sidesStringifyList = "List of sides:\n";
+
+        foreach (var side in sides)
+        {
+            sidesStringifyList += $"title:{side.title}, price: {side.price} sek.\n";
+        }
+
+        return $"{mainsStringifyList} - {sidesStringifyList}";
+
+    }
+
+    public class Food
+    {
+        public string _id { get; set; } = string.Empty;
+        public string title { get; set; } = string.Empty;
+        public int price { get; set; }
+        public string description { get; set; } = string.Empty;
+        public List<string> categories { get; set; } = [];
+
+    }
+}
